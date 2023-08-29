b63f05f473b2b0aedf40b4dc0d50a928febf6a0e
Alireza Choobakzan <135063405+AlirezaChx@users.noreply.github.com>
2023-08-29 16:31:31 +0330

    dad

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Threading.Channels;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("");
+
+for (int i = 0; i <= 5; i++)
+{
+    Console.WriteLine(i);
+}
+
